162e7c3a6d8ed489c3b11c2204c92c88acfd4c56
Loaded Poke <loadedpoke@gmail.com>
2020-03-10 17:10:23 -0500

    Create model post.js

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Post', schema);
