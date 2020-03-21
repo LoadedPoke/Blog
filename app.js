1e14310dabf3532fbf3ae16ef56092d8d46b50a2
Loaded Poke <loadedpoke@gmail.com>
2020-03-21 18:31:17 -0500

    Add error handler to all posts searching

--- app.js
+++ app.js
@@ -11,17 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    Post.find({}).then(posts => {
-        res.render('index', {posts: posts});
-    });
+    Post.find({}).then(
+        posts => {
+            res.render('index', {posts})
+        },
+        err => {
+            res.status(200).json({err})
+        }
+    );
 });
 
 app.get('/create', (req, res) => res.render('create'));
 app.post('/create', (req, res) => {
     const {title, body} = req.body;
     Post.create({
-        title: title,
-        body: body
+        title,
+        body
     }).then(post => console.log(post._id));
     res.redirect('/');
 });
